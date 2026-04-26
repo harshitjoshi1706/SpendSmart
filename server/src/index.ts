d73b4bce04571948bf6a394b53868c1a26cfb32c
Harshit Joshi <harshitjoshi@Harshits-MacBook-Air.local>
2026-04-26 22:12:40 +0530

    fix index.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 
 const allowedOrigins: string[] = [
-  "http://localhost:5173",
+  "http://localhost:3000",
 ];
 
 if (process.env.CLIENT_URL) {
